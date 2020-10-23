864d0349ed2a78114b148d3fe1e8d5fc3db78961
Thuat Nguyen <73330731+thuatnguyen6789@users.noreply.github.com>
2020-10-23 14:24:18 +0700

    Add files via upload

---
+++ Lab01/bai2 vd2.c
@@ -0,0 +1,9 @@
+main()
+{
+	char abc; /*abc of type character */
+	int xyz; /*xyz of type integer */
+	float length; /*length of type float */
+	double area; /* area of type double */
+	long liteyrs; /* liteyrs of type long int */
+	short arm; /*arm of type short integer*/
+}
